5aec390f12164d26ac94a82c33d33e91c1ad6c25
Philippe Trudel <p.trudel1996@gmail.com>
2018-05-27 01:11:35 -0400

    fixed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,18 @@
 module.exports = {
     "extends": "airbnb",
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    'rules': {
+        'strict': 'off',
+        'indent': ['error', 2, { 'SwitchCase': 1 }],
+        'array-bracket-spacing': ['error', 'always'],
+        'comma-dangle': ['error', 'never'],
+        'id-length': 'off',
+        'quotes': ['warn', 'single'],
+        'jsx-quotes': ['error', 'prefer-single'],
+        "semi": [2, "always"]
+    }
 };
