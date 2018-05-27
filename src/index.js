2ca12c0d7be08c7acef4698fdf1858398b943d9c
Philippe Trudel <p.trudel1996@gmail.com>
2018-05-27 16:43:07 -0400

    semantic-ui integration for images and icons

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App from './components/App';
 
 import './style/layout.css';
 import './style/style.scss';
+import '../node_modules/semantic-ui-css/semantic.css'
 
 const render = Component =>
   ReactDOM.render(
